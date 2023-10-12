56167ae217a0c6f8202716ff617efbc89904c74a
Emilio-Plances <emilio.plances@gmail.com>
2023-10-12 22:30:03 +0200

    completamento

--- m1/s3/g4/esercizio-java-tombola/js/script.js
+++ m1/s3/g4/esercizio-java-tombola/js/script.js
@@ -38,8 +38,8 @@ rollButton.addEventListener(`click`,function(){
 
     controlEvent(p.innerText);
     
-    tombolaBody.childNodes[Number(p.innerText)-1].style.backgroundColor=`#c7161f`;
-    tombolaBody.childNodes[Number(p.innerText)-1].style.color=`rgb(242, 243, 216)`;
+    tombolaBody.childNodes[p.innerText-1].style.backgroundColor=`#c7161f`;
+    tombolaBody.childNodes[p.innerText-1].style.color=`rgb(242, 243, 216)`;
 });
 
 numberInside.append(p);
@@ -47,6 +47,41 @@ numberOut.append(numberInside);
 number.append(numberOut,rollButton);
 tombolaHeader.append(h3,number);
 
+
+
+
+/***************************************
+ *  Zona Giocatore Header
+ */
+
+let playerZone=document.querySelector(`.player-zone`);
+let playerTab=document.createElement(`div`);
+let playerHeader=document.createElement(`div`);
+let h6=document.createElement(`h6`);
+
+
+playerTab.classList.add(`player-tab`);
+playerHeader.classList.add('playerHeader');
+h6.innerText=`TOMBOLONE`;
+
+playerHeader.append(h6);
+
+/***************************************
+ *  Zona Giocatore Body 
+ * se avessi usato dei blocchi sarebbe
+ * stato più facile ma volevo costruire una tabella :P
+ */
+
+let playerNum=document.createElement("table");
+let tbody=document.createElement(`tbody`);
+
+playerNum.classList.add(`playerNum`);
+
+numberPlayerCicle();
+
+playerNum.append(tbody);
+playerTab.append(playerHeader,playerNum);
+playerZone.append(playerTab);
 
 function controlEvent(p){
 
@@ -85,6 +120,37 @@ function generateNumeroTombola(){
     tombola.append(tombolaHeader,tombolaBody);
 }
 
+function numberPlayerCicle(){
+    for(let i=0;i<4;i++){
+        let tr=document.createElement(`tr`);
+        
+        for(let k=0;k<6;k++){
+    
+            let playerNumber=document.createElement(`td`);
+            playerNumber.innerText=Math.floor(Math.random()*90)+1;
+            playerNumber.classList.add(`playerNumber`);
+            
+            if(k==0){
+                playerNumber.classList.add('no-left');
+            }
+    
+            if(i==4){
+                playerNumber.classList.add(`no-bottom`);
+            }
+            
+            tr.append(playerNumber);
 
-
-
+            playerNumber.addEventListener(`click`,()=>{
+                if(playerNumber.style.backgroundColor){
+                    playerNumber.style.backgroundColor=``;
+                    playerNumber.style.color=``;
+                }else{
+                playerNumber.style.backgroundColor=`#c7161f`;
+                playerNumber.style.color=`rgb(242, 243, 216)`;
+                }
+            })
+        }
+        
+        tbody.append(tr);
+    }
+}
