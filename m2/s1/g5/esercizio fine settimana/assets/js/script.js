6f3a32f93e19be66000cdfac488adeaf61fc3e07
Emilio-Plances <emilio.plances@gmail.com>
2023-10-27 17:15:07 +0200

    yeeeeeeeeeee

--- m2/s1/g5/esercizio fine settimana/assets/js/script.js
+++ m2/s1/g5/esercizio fine settimana/assets/js/script.js
@@ -30,12 +30,12 @@ function apriChiudiPopup() {
 
 /*___________________________Creazione pop-up Segnalibro___________________________________________*/
 
-// buttonInfoPost.addEventListener(`click`,{
-//    Swal.fire({
-//       position: 'top-end',
-//       icon: 'success',
-//       title: 'Your work has been saved',
-//       showConfirmButton: false,
-//       timer: 1500
-//     })
-// })
+buttonInfoPost.addEventListener(`click`,()=>{
+   Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'Your work has been saved',
+      showConfirmButton: false,
+      timer: 1500
+    })
+})
