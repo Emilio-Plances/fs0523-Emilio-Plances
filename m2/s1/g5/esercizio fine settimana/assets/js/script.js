2e7f10c6048c9b54a3972d554c8e1a64f78880ed
Emilio-Plances <emilio.plances@gmail.com>
2023-10-27 16:45:22 +0200

    Creazione popup fatta

--- m2/s1/g5/esercizio fine settimana/assets/js/script.js
+++ m2/s1/g5/esercizio fine settimana/assets/js/script.js
@@ -1,6 +1,9 @@
 let body=document.querySelector(`body`);
 let header=document.querySelector(`#header`);
 let headerButton=header.querySelector(`#nav-button`);
+let popup=document.querySelector(`#popup`);
+let login=document.querySelector(`#login`);
+let closePopup=document.querySelector(`#exit`);
 let heroButton=document.querySelector(`#hero-button`);
 
 /*_________________________________Cambio header scrollando_________________________________*/
@@ -15,3 +18,13 @@ window.addEventListener(`scroll`,()=>{
 
 /*_____________________________________Creazione pop-up___________________________________________*/
 
+headerButton.addEventListener(`click`,apriChiudiPopup);
+heroButton.addEventListener(`click`,apriChiudiPopup);
+popup.addEventListener(`click`,apriChiudiPopup);
+closePopup.addEventListener(`click`,apriChiudiPopup);
+function apriChiudiPopup() {
+   popup.classList.toggle(`hidden`);
+   login.classList.toggle(`hidden`);
+}
+
+/*___________________________Creazione pop-up Segnalibro___________________________________________*/
