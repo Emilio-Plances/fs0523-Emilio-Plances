1ff2792aa047fe2139ac552fde638024f3e14d97
Emilio-Plances <emilio.plances@gmail.com>
2023-11-07 15:32:47 +0100

    Storages

---
+++ m2/s3/g2/esercizio-localStorage/js/script.js
@@ -0,0 +1,47 @@
+let previousInput=document.querySelector(`#previous-input`);
+let saveButton = document.querySelector(`#save`);
+let deleteButton = document.querySelector(`#delete`);
+let inputName=document.querySelector(`#input-name`);
+let i= localStorage.getItem(`index`)!=null ? localStorage.getItem(`index`)-1 : 1;
+let counter=sessionStorage.getItem(`timer`)!=null? sessionStorage.getItem(`timer`):0;
+
+previousInput.innerText=localStorage.getItem(`nome${i}`);
+class getName{
+   constructor(name,id){
+      this.id = id;
+      this.name = name;
+      this.saveThisName();
+   }
+
+   saveThisName(){
+      localStorage.setItem(`nome${this.id}`,this.name);
+   }
+}
+
+saveButton.addEventListener(`click`, (e) => {
+   e.preventDefault();
+   if(inputName.value==``){
+      alert(`Inserire un campo`);
+      return;
+   }
+   new getName(inputName.value,i);
+   previousInput.innerText=localStorage.getItem(`nome${i}`);
+   i++;
+   inputName.value=``;
+   localStorage.setItem(`index`,i);
+})
+
+deleteButton.addEventListener(`click`, (e) => {
+   e.preventDefault();
+   i= i>1 ? i-1: i=1;
+   localStorage.removeItem(`nome${i}`);
+   previousInput.innerText=localStorage.getItem(`nome${i-1}`);
+   localStorage.setItem(`index`,i);
+   
+})
+
+setInterval(() => {
+   counter++;
+   sessionStorage.setItem(`timer`,counter);
+   document.querySelector(`#timer`).innerText=counter;
+},1000)
