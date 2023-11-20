169b1814d6af1f9a2c2017696509ff7577e6cb58
Emilio-Plances <emilio.plances@gmail.com>
2023-11-20 15:47:42 +0100

    riveduto e corretto ancora

--- m3/s1/g1/esercizio-typescript/js/app.js
+++ m3/s1/g1/esercizio-typescript/js/app.js
@@ -4,30 +4,23 @@ class UsefulFunctions {
         let randomNumber = Math.floor(Math.random() * 100) + 1;
         let players = this.getPlayers();
         let [g1, g2] = players;
-
         console.log(`Il numero uscito è il ${randomNumber}`);
-
         if (parseInt(g1.number) == parseInt(g2.number) && parseInt(g1.number) == randomNumber)
             return `State palesemente barando`;
-
-        if (parseInt(g1.number) == randomNumber)
+        else if (parseInt(g1.number) == randomNumber)
             return `Complimenti ${g1.name} hai indovinato!`;
-
-        if (parseInt(g2.number) == randomNumber)
+        else if (parseInt(g2.number) == randomNumber)
             return `Complimenti ${g2.name} hai indovinato!`;
-
-        
-        let diff1;
-        let diff2;
-        diff1 = Math.abs(randomNumber - parseInt(g1.number));
-        diff2 = Math.abs(randomNumber - parseInt(g2.number));
-        if (diff1 == diff2)
-            return `Questo è un pareggio!`;
-        return `Nessuno di voi due ha indovinato ma si è avvicinato di più ${diff1 < diff2 ? g1.name : g2.name}!`;
-        
+        else {
+            let diff1;
+            let diff2;
+            diff1 = Math.abs(randomNumber - parseInt(g1.number));
+            diff2 = Math.abs(randomNumber - parseInt(g2.number));
+            if (diff1 = diff2)
+                return `Questo è un pareggio!`;
+            return `Nessuno di voi due ha indovinato ma si è avvicinato di più ${diff1 < diff2 ? g1.name : g2.name}!`;
+        }
     }
-
-    
     getPlayers() {
         let g1 = document.querySelector(`#g1`);
         let g2 = document.querySelector(`#g2`);
